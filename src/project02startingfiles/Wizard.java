babc6123b6d899ab1b973aaf1e05309679a162ef
jl9904 <s550715@nwmissouri.edu>
2023-11-26 19:09:06 -0600

    Thief and Wizard Class

---
+++ src/project02startingfiles/Wizard.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project02startingfiles;
+
+/**
+ *
+ * @author s550715
+ */
+public class Wizard extends Player {
+
+    public String getSpecialMove() {
+        return "Cast a Spell";
+    }
+
+    public void useSpecialMove() {
+
+    }
+}
