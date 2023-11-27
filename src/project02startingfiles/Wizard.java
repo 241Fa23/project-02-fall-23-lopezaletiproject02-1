b68106aed40c74710a005e53228c0a017289e1b2
RathanAleti <aletirathanreddy77@gmail.com>
2023-11-27 11:15:01 -0600

    wizard class

--- src/project02startingfiles/Wizard.java
+++ src/project02startingfiles/Wizard.java
@@ -6,6 +6,7 @@ package project02startingfiles;
 
 /**
  *
+<<<<<<< Updated upstream
  * @author s550715
  */
 public class Wizard extends Player {
@@ -13,8 +14,12 @@ public class Wizard extends Player {
     public String getSpecialMove() {
         return "Cast a Spell";
     }
-
-    public void useSpecialMove() {
-
+     
+    public void useSpecialMove(){
+        System.out.println("wizard uses special moves Cast a Spell");
     }
+    public String toString (){
+        return "wizard"+super.toString();
+
+}
 }
