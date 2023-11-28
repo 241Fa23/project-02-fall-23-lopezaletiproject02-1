31a90de748249ec70d78c19ac1137f2a499726c3
jl9904 <s550715@nwmissouri.edu>
2023-11-28 01:13:17 -0600

    started working on the attack method

--- src/project02startingfiles/Project02StartingFiles.java
+++ src/project02startingfiles/Project02StartingFiles.java
@@ -16,16 +16,17 @@ public class Project02StartingFiles {
     /**
      * @param args the command line arguments
      */
+    //Import the scanner & random for all methods
+    private static final Scanner input = new Scanner(System.in);
+    private static final Random rand = new Random();
+
+    //universal predefined character health and scores
+    private static Wizard wiz = new Wizard();
+    private static Knight kni = new Knight();
+    private static Healer heal = new Healer();
+    private static Thief thef = new Thief();
+
     public static void main(String[] args) {
-
-        //predefined character health and scores
-        Wizard wiz = new Wizard();
-        Knight kni = new Knight();
-        Healer heal = new Healer();
-        Thief thef = new Thief();
-
-        //Import the scanner for user input
-        Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to JavaQuest!\nStay alive and increase your score!");
         System.out.print("Choose a character...\n{k}Knight || {h}Healer || {w}Wizard || {t}Thief \n>>");
@@ -49,7 +50,6 @@ public class Project02StartingFiles {
         System.out.print("{?}Status Report? || {n}{s}{e}{w} Move 1 Space North, South, East, or West || {q}Quit\n>>");
         String choice = input.nextLine();
 
-        BenignScenes();
         while (choice != "q") {
             switch (choice) {
                 case "?":
@@ -74,8 +74,18 @@ public class Project02StartingFiles {
 
     //A method that prints out the players status report
     //A method that generates whether the player has a benign scene or a attack scenario
+    public static void scearioScene() {
+        int typeOfScene = rand.nextInt(5);
+
+        if (typeOfScene == 0) {
+            //initiate attack scenario method
+        } else {
+            runBenignScenes();
+        }
+    }
+
     //A method that has the benign scenes in it.
-    public static void BenignScenes() {
+    public static void runBenignScenes() {
         System.out.println("**********************");
         Random rand = new Random();
         int option = rand.nextInt(1, 4);
@@ -83,19 +93,34 @@ public class Project02StartingFiles {
             case 1:
                 System.out.println("Nothing here...\n**********************");
                 break;
-
             case 2:
                 System.out.println("Nice trees around here...\n**********************");
                 break;
-
             case 3:
                 System.out.println("Interesting cottage...\n**********************");
                 break;
-
             case 4:
                 System.out.println("Potty break...\n**********************");
                 break;
+        }
+        wiz.setScore(wiz.getScore() + 1);
+        kni.setScore(kni.getScore() + 1);
+        heal.setScore(heal.getScore() + 1);
+        thef.setScore(thef.getScore() + 1);
+    }
 
+    //Method that has the attack scenarios
+    public static void attackScenario() {
+        String[] foes = {"zombie", "bandit", "lobbyist"};
+        String typeOfFoe = foes[rand.nextInt(foes.length)];
+        System.out.println("Oh no! You are being attacked by a " + typeOfFoe + "!\nHow would you like to handle this?");
+        System.out.print("\n{s}Special Move || {r}Run!\n>>");
+        String choice = input.next().toLowerCase();
+
+        if (choice.equals("s")) {
+            if () {
+                
+            }
         }
     }
 }
